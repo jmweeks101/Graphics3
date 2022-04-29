6d869f0481b14fe0020f670e2a43c22caa55c961
Josh Weeks <jmweeks101@gmail.com>
2022-04-28 23:41:59 -0700

    finished label, getCenter, zoom methods for base3D shape class  and updates to base3D.py and GUI.py

--- GUI.py
+++ GUI.py
@@ -4,15 +4,59 @@ from base3D import *
 def main():
     win = GraphWin("Graphics3",500,500)
     win.setCoords(0,0,10,10)
+    testPrisim(win)
+    win.close()
 
-    pyramid = Pyramid(win,Point(6,6),5,5,2)
+def testPrisim(win):
+    cube = Prisim(win,Point(4,4),2,2,2)
+    cube.show()
+    win.getMouse()
+    #fill color
+    cube.fill("blue")
+    win.getMouse()
+    #move
+    cube.translate(-1,1)
+    win.getMouse()
+    #make label
+    cube.setLabel("Our cube","red",20,0,0)
+    cube.drawLabel()
+    win.getMouse()
+    #customize edges
+    cube.setEdgeColor("green")
+    cube.setEdgeSize(3)
+    win.getMouse()
+    #clone
+    cube2 = cube.clone()
+    cube2.translate(4,2)
+    cube2.show()
+    win.getMouse()
+    #zoom
+    cube.zoom(2)
+    cube.setLabel("Our cube","red",20,0,0)
+    cube.drawLabel()
+    win.getMouse()
+
+def testPyramid(win):
+    pyramid = Pyramid(win,Point(6,6),5,4,2)
     pyramid.show()
+    win.getMouse()
+    #fill color
     pyramid.fill("blue")
     win.getMouse()
+    #move
     pyramid.translate(-1,1)
     win.getMouse()
-    pyramid.drawLabel("Our Pyramid","red",20)
-    win.close()
+    #make label
+    pyramid.setLabel("Our Pyramid","red",20,0,2)
+    pyramid.drawLabel()
+    win.getMouse()
+    #customize edges
+    pyramid.setEdgeColor("red")
+    pyramid.setEdgeSize(3)
+    win.getMouse()
+    #hide
+    pyramid.hide()
+    win.getMouse()
 
 
 if __name__ == "__main__":
--- base3D.py
+++ base3D.py
@@ -1,5 +1,6 @@
 from parallelogram import create3
 from pyramid import *
+import numpy as np
 
 #base shape class, will define all 3d shape subclasses
 class base3D:
@@ -7,34 +8,102 @@ class base3D:
     def __init__(self,win,faces):
         self.win = win
         self.faces = faces
-
-    #returns enter of object
-    def getCenter(self):
-        #take center of each face and make a polygon from those points
-        #then find center of that new polygon
-        ps = []
+        self.fillColor = None
+        self.edgeColor = None
+        self.edgeSize = None
+        self.points = []
+        #go through each face
         for face in self.faces:
-            ps.append(face.getCenter())
-        x = Polygon(ps)
-        return x.getCenter()
+            #find each point in each face
+            try:
+                self.points = self.points + face.getPoints()
+            #for when there is a line in self.faces
+            except:
+                self.points = self.points + [face.getCenter()]
+
+    #update self.points
+    def updatePoints(self):
+        self.points = []
+        #go through each face
+        for face in self.faces:
+            #find each point in each face
+            try:
+                self.points = self.points + face.getPoints()
+            #for when there is a line in self.faces
+            except:
+                self.points = self.points + [face.getCenter()]
+
+    #returns cnter of the object
+    def getCenter(self):
+        points = []
+        xList = []
+        yList = []
+        #take out duplicate points
+        points2 = []
+        for point in self.points:
+            if point not in points2:
+                points2.append(point)
+        #put the x and y coordinates of each point in seperate lists
+        for point in points2:
+            xList.append(point.getX())
+            yList.append(point.getY())
+        #retiurn the center by taking the average of the two lists
+        return Point(sum(xList)/len(xList),sum(yList)/len(yList))
 
     #translate shape in x,y direction
     def translate(self,x,y):
         self.undrawLabel()
         for face in self.faces:
             face.move(x,y)
+        self.updatePoints()
 
     #makes pbject apear closer or further away(shrink or grow shape)
     def zoom(self,z):
         self.undrawLabel()
-        pass
+        #find the current center and undraw current shape
+        center = self.getCenter()
+        self.hide()
+        #list to store new faces
+        faces2 = []
+        for face in self.faces:
+            points = face.getPoints()
+            points2 = []
+            for point in points:
+                #for each point in each face, find the relative position to the center in x and y
+                if point.getX()-center.getX()>0:
+                    x = 1
+                else:
+                    x = -1
+                if point.getY()-center.getY()>0:
+                    y = 1
+                else:
+                    y = -1
+                #create a new point that is zoomed in/out from the original
+                points2.append(Point(point.getX()+z*np.cos(np.radians(45))*x,point.getY()+z*np.sin(np.radians(45))*y))
+            faces2.append(Polygon(points2))
+        #save new faces to self.faces
+        self.faces = faces2
+        #add back customizations if any
+        try:
+            self.fill(self.fillColor)
+        except:
+            pass
+        try:
+            self.setEdgeSize(self.edgeSize)
+        except:
+            pass
+        try:
+            self.setEdgeColor(self.edgeColor)
+        except:
+            pass
+        self.show()
+        self.updatePoints()
 
     #rotate the object in 3D space
     def rotate(self,thetaX,thetaY,thetaZ):
         self.undrawLabel()
-        for face in self.faces:
-            for point in face:
-                pass
+        pass
+        self.updatePoints()
 
     #draw shape to window
     def show(self):
@@ -51,23 +120,33 @@ class base3D:
     def fill(self,color):
         for face in self.faces:
             face.setFill(color)
+        self.fillColor = color
 
     #set edge color
-    def edgeColor(self,color):
+    def setEdgeColor(self,color):
         for face in self.faces:
             face.setOutline(color)
+        self.edgeColor = color
 
     #set edge pixel width
-    def edgeSize(self,size):
+    def setEdgeSize(self,size):
         for face in self.faces:
             face.setWidth(size)
+        self.edgeSize = size
 
-    #displays a label custom label over shape
-    def drawLabel(self,text,color,size):
+    #customize label
+    def setLabel(self,text,color,size,x,y):
         self.label = Text(self.getCenter(),text)
         self.label.setTextColor(color)
-        self.label.setTextSize(size)
-        self.draw(self.win)
+        self.label.setSize(size)
+        self.label.move(x,y)
+
+    #displays a label custom label over shape
+    def drawLabel(self):
+        try:
+            self.label.draw(self.win)
+        except:
+            pass
 
     #undraws label
     def undrawLabel(self):
@@ -78,7 +157,7 @@ class base3D:
 
 #class to create a prisim
 class Prisim(base3D):
-    def __init__(self,win,baseCenter,width,height,depth):
+    def __init__(self,win,baseCenter,height,width,depth):
         self.win = win
         self.baseCenter = baseCenter
         self.width = width
@@ -89,7 +168,7 @@ class Prisim(base3D):
 
     #returns a clone of the prisim
     def clone(self):
-        return Prisim(self.win,self.baseCenter,self.width,self.height,self.depth)
+        return Prisim(self.win,self.baseCenter,self.height,self.width,self.depth)
 
 #class to create a pyramid
 class Pyramid(base3D):
@@ -101,6 +180,10 @@ class Pyramid(base3D):
         self.sideLength = sideLength
         #create base shape
         base3D.__init__(self,self.win,makePyramid(self.tip, self.height, self.baseSides, self.sideLength))
+
+    #returns a clone of the prisim
+    def clone(self):
+        return Pyramid(self.win,self.tip,self.height,self.baseSides,self.sideLength)
 
 #class to create a sphere
 class Sphere(base3D):
