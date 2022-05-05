7a19800569ed6777b62dc6cf0621844da77d61fa
Joshua Miller <joshmiller5472@gmail.com>
2022-05-05 15:16:52 -0700

    created working test file for all 3 objects

--- sphere.py
+++ sphere.py
@@ -5,7 +5,8 @@ class Sphere:
     #Takes inputs of the center, radius, and height of sphere, as well as the rgb values (as a list) for the sphere
     #Side parameter determines which way the light is coming from, default is the right side.
     #Note: coordinates have to be set on graphics window for it to work properly.
-    def __init__(self, center, radius, rgb, side='right'):
+    def __init__(self, win, center, radius, rgb, side='right'):
+        self.win = win
         self.side = side
         self.center = center
         self.radius = radius
@@ -26,6 +27,7 @@ class Sphere:
             gradient = Circle(Point(self.center.x-self.radius/4, self.center.y+self.radius/4), self.radius/1.6)
         gradient.setOutline(color_rgb(self.rgb[0],self.rgb[1],self.rgb[2]))
         self.gradientrgb = [self.rgb[0]+5,self.rgb[1]+5,self.rgb[2]+5]
+        #Used for loop to avoid value exceding 255
         for i, color in enumerate(self.gradientrgb):
             if color>255:
                 self.gradientrgb[i] = 255
@@ -39,6 +41,7 @@ class Sphere:
         else:
             highlight = Oval(Point(self.center.x-self.radius/2.7, self.center.y+self.radius/1.6), Point(self.center.x-self.radius/2.0, self.center.y+self.radius/2.2))
         rgb = [self.rgb[0]+130,self.rgb[1]+130,self.rgb[2]+130]
+        #Used for loop to avoid value exceding 255
         for i, color in enumerate(rgb):
             if color>255:
                 rgb[i] = 0
@@ -58,16 +61,17 @@ class Sphere:
     #Takes the four objects and adds them in a list.
     def createSphere(self):
         sphere = [self.createShadow(), self.createCircle(), self.createGradient(), self.createHighlight()]
-        return sphere
+        for face in sphere:
+            face.draw(self.win)
 
 
 
 def main():
     win = GraphWin('', 500, 500)
     win.setCoords(0,0,10,10)
-    sphere = Sphere(Point(5,5), 2, [100,100,100], side='left')
-    for face in sphere.createSphere():
-        face.draw(win)
+    sphere = Sphere(win, Point(5,5), 2, [100,100,100], side='left')
+    sphere.createSphere()
     win.getMouse()
 
-main()
+if __name__ == '__main__':
+    main()
