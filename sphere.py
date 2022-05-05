e16309d46a24d9b1eecf7281c11767a178c0edf6
Josh Miller <joshmiller5472@gmail.com>
2022-05-04 19:49:47 -0700

    Added the ability to switch sphere sides

--- sphere.py
+++ sphere.py
@@ -2,22 +2,28 @@ from graphics import *
 
 #Creates a sphere object
 class Sphere:
-    #Takes inputs of the center, radius, and height of sphere, as well as the rgb values for the sphere
-    def __init__(self, win, center, radius, rgb):
-        self.win = win
+    #Takes inputs of the center, radius, and height of sphere, as well as the rgb values (as a list) for the sphere
+    #Side parameter determines which way the light is coming from, default is the right side.
+    #Note: coordinates have to be set on graphics window for it to work properly.
+    def __init__(self, center, radius, rgb, side='right'):
+        self.side = side
         self.center = center
         self.radius = radius
         self.rgb = rgb
 
-
+    #Creates the overall sphere as a circle
     def createCircle(self):
         circle = Circle(self.center, self.radius)
         circle.setFill(color_rgb(self.rgb[0],self.rgb[1],self.rgb[2]))
         circle.setOutline(color_rgb(self.rgb[0],self.rgb[1],self.rgb[2]))
         return circle
 
+    #creates a smaller circle to apply a gradience, making it appear more 3D
     def createGradient(self):
-        gradient = Circle(Point(self.center.x+self.radius/4, self.center.y-self.radius/4), self.radius/1.6)
+        if self.side == 'right':
+            gradient = Circle(Point(self.center.x+self.radius/4, self.center.y+self.radius/4), self.radius/1.6)
+        else:
+            gradient = Circle(Point(self.center.x-self.radius/4, self.center.y+self.radius/4), self.radius/1.6)
         gradient.setOutline(color_rgb(self.rgb[0],self.rgb[1],self.rgb[2]))
         self.gradientrgb = [self.rgb[0]+5,self.rgb[1]+5,self.rgb[2]+5]
         for i, color in enumerate(self.gradientrgb):
@@ -26,8 +32,12 @@ class Sphere:
         gradient.setFill(color_rgb(self.gradientrgb[0],self.gradientrgb[1],self.gradientrgb[2]))
         return gradient
 
+    #Creates a highlight to make it appear even more 3D
     def createHighlight(self):
-        highlight = Oval(Point(self.center.x+self.radius/2.7, self.center.y-self.radius/1.6), Point(self.center.x+self.radius/2.0, self.center.y-self.radius/2.2))
+        if self.side == 'right':
+            highlight = Oval(Point(self.center.x+self.radius/2.7, self.center.y+self.radius/1.6), Point(self.center.x+self.radius/2.0, self.center.y+self.radius/2.2))
+        else:
+            highlight = Oval(Point(self.center.x-self.radius/2.7, self.center.y+self.radius/1.6), Point(self.center.x-self.radius/2.0, self.center.y+self.radius/2.2))
         rgb = [self.rgb[0]+130,self.rgb[1]+130,self.rgb[2]+130]
         for i, color in enumerate(rgb):
             if color>255:
@@ -36,31 +46,28 @@ class Sphere:
         highlight.setOutline(color_rgb(self.gradientrgb[0],self.gradientrgb[1],self.gradientrgb[2]))
         return highlight
 
+    #Creates the shadow that the sphere would create.
     def createShadow(self):
-        shadow = Oval(Point(self.center.x+0.25*self.radius, self.center.y+self.radius/.9), Point(self.center.x-2*self.radius,self.center.y+self.radius/1.66))
-        # rgb = [self.rgb[0]-60,self.rgb[1]-60,self.rgb[2]-60]
-        # for i, color in enumerate(rgb):
-        #     if color<0:
-        #         rgb[i] = 0
+        if self.side == 'right':
+            shadow = Oval(Point(self.center.x+0.25*self.radius, self.center.y-self.radius/.9), Point(self.center.x-2*self.radius,self.center.y-self.radius/1.66))
+        else:
+            shadow = Oval(Point(self.center.x-0.25*self.radius, self.center.y-self.radius/.9), Point(self.center.x+2*self.radius,self.center.y-self.radius/1.66))
         shadow.setFill(color_rgb(30,30,30))
         return shadow
 
-    def drawSphere(self):
-        shadow = self.createShadow()
-        shadow.draw(self.win)
-        circle = self.createCircle()
-        circle.draw(self.win)
-        gradient = self.createGradient()
-        gradient.draw(self.win)
-        highlight = self.createHighlight()
-        highlight.draw(self.win)
+    #Takes the four objects and adds them in a list.
+    def createSphere(self):
+        sphere = [self.createShadow(), self.createCircle(), self.createGradient(), self.createHighlight()]
+        return sphere
+
 
 
 def main():
     win = GraphWin('', 500, 500)
-
-    sphere = Sphere(win, Point(400, 250), 100,[100,100,100])
-    sphere.drawSphere()
+    win.setCoords(0,0,10,10)
+    sphere = Sphere(Point(5,5), 2, [100,100,100], side='left')
+    for face in sphere.createSphere():
+        face.draw(win)
     win.getMouse()
 
 main()
